af73ffb87c459fff75409a1506e20f25077d1813
Fahima <232140@astanait.edu.kz>
2024-11-05 22:58:53 +0600

    help in Creating CLI class to provide a command-line interface for user interactions

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// src/Main.java
+import view.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize the CLI (Command Line Interface) and start the main menu
+        CLI cli = new CLI();
+        cli.mainMenu();
+    }
+}
